30d2a7d817d7e93f4bb994c1adb758812e49fc33
Patricia Eliseu <patyeliseu4@gmail.com>
2024-09-25 18:06:48 -0300

    add cap1 e 2

---
+++ src/main/java/exemplo/cap1/Exemple1.java
@@ -0,0 +1,7 @@
+package exemplo.cap1;
+
+public class Exemple1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/exemplo/cap1/Exemplo2.java
@@ -0,0 +1,7 @@
+package exemplo.cap1;
+
+public class Exemplo2 {
+    public static void main(String[] args) {
+
+    }
+}
